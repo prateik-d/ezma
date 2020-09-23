038c3380666ddf8b49a7aac9032a252d225e359d
Prateik <pratik.dhotmal@gmail.com>
2020-09-23 10:44:17 +0530

    upload to server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://127.0.0.1:8000/'
+  // baseUrl: 'http://127.0.0.1:8000/'
+  baseUrl: 'http://15.206.66.117/ezymp/index.php/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://127.0.0.1:8000/'
+  // baseUrl: 'http://127.0.0.1:8000/'
+  baseUrl: 'http://15.206.66.117/ezymp/index.php/'
 };
 
 /*
